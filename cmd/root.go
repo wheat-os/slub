f1029ce28c586732150039f3e1ffce7b00f245cd
bandl <1658002533@qq.com>
2022-05-27 23:18:46 +0800

    feat(slub): add slub desc

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "slub",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Slub is a scaffolding for efficient development of the slubby framework",
+	Long: `Slub allows you to build a go mod-based, 
+structured crawler framework, and slub provides a 
+number of commands to implement a structured calling method, 
+using -h to get more help with slub usage.
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
